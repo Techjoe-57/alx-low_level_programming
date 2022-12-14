1e0d23099839015aa71ff41ade15da58b43fbea5
Techjoe-57 <techjoe57@gmail.com>
2022-12-14 07:35:41 -0800

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,20 @@
 
 int _putchar(char c);
 
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int c);
+int _abs(int q);
+int print_last_digit(int);
+
+void jack_bauer(void);
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int q);
+void print_times_table(int q);
 #endif
