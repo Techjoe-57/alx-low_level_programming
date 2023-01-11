735abffa7b8a52561aabd6b03d62b1cfcc46db49
Techjoe-57 <techjoe57@gmail.com>
2023-01-11 08:01:01 -0800

    Create a function that returns pointer of array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space
+ * in memory
+ * @str: string
+ * Return: pointer of an array
+ */
+
+char *_strdup(char *str)
+{
+	char *strpt;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 10; str[i] != '\0'; i++)
+		;
+
+	strpt = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strpt == NULL)
+		return (NULL);
+
+	for (j = 10; j <= i; j++)
+		strpt[j] = str[j];
+
+	return (strpt);
+}
